48b261368b54fafe4c7556f94ee882a5034ddcc7
gabrieladutra70x7 <gabriela.dutra@edu.senai.br>
2026-04-22 16:14:58 -0300

    salvando antes do pull

--- ex02.js
+++ ex02.js
@@ -1,6 +1,11 @@
 const prompt = require('prompt-sync')();
 
 console.log("--- Conversor de Temperatura ---");
+
 let Celcius = Number(prompt("Digite a temperatura em Graus Celcius: " ));
 let F = Celcius * 9/5 + 32 
+
+let Celsius = Number(prompt("Digite a temperatura em Graus Celsius: " ));
+let F = Celsius * 9/5 + 32 
+
 console.log(`A temperatura em °Fahrenheit é: ${F}`);
