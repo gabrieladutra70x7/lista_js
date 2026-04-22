c8c5d66896632921030a118479c4538bb64df380
Gabriela Gimenez Dutra <gabriela.dutra@edu.senai.br>
2026-04-22 15:30:51 -0300

    Update ex02.js

--- ex02.js
+++ ex02.js
@@ -1,6 +1,6 @@
 const prompt = require('prompt-sync')();
 
-console.log("--- Soma de Valores ---");
+console.log("--- Conversor de Temperatura ---");
 let Celcius = Number(prompt("Digite a temperatura em Graus Celcius: " ));
 let F = Celcius * 9/5 + 32 
 console.log(`A temperatura em °Fahrenheit é: ${F}`);
